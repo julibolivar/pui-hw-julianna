b7cc6413029d746e3293322eaa119163de964a27
julibolivar <122954826+julibolivar@users.noreply.github.com>
2023-09-26 10:21:08 -0400

    hw3

--- solution-hw-3/app.js
+++ solution-hw-3/app.js
@@ -1,96 +1,83 @@
-const basePrice = 2.49;
-
 let glazeOptions = [
     {
         glaze: 'Keep Original',
-        glazeAdd: '0'
+        priceAdd: 0
     },
     {
         glaze: 'Sugar Milk',
-        glazeAdd: '0'
+        priceAdd: 0
     },
     {
         glaze: 'Vanilla Milk',
-        glazeAdd: '0.5',
+        priceAdd: 0.50
     },
     {
         glaze: 'Double Chocolate',
-        glazeAdd: '1.5',
+        priceAdd: 1.50
     }
-];
+  ];
 
-let selectGlaze = document.getElementById("glazing")
-for (index in glazeOptions) {
-    selectGlaze.options[selectGlaze.options.length] = new Option(glazeOptions[index].glaze);
-    console.log("Populating Glazing Menu");
-}
-
-let sizeOptions = [
+  let packSizes = [
     {
-        size: '1',
-        sizeMult: '1'
+        packSize: '1',
+        priceMultiply: 1
     },
     {
-        size: '3',
-        sizeMult: '3'
+        packSize: '3',
+        priceMultiply: 2
     },
     {
-        size: '6',
-        sizeMult: '5',
+        packSize: '6',
+        priceMultiply: 5
     },
     {
-        size: '12',
-        sizeMult: '10',
+        packSize: '12',
+        priceMultiply: 10
     }
-];
-//   rolls and price adaptations
+  ];
+
+let selectElement = document.querySelector('#glazing');
+let selectElement2 = document.querySelector('#pack-size');
+let basePriceElement = document.querySelector('#detail-item-price');
+let selectedGlazingIndex = 0; // start with the first glazing
+let selectedPackIndex = 0; // start with the first pack size
+  
+document.addEventListener('DOMContentLoaded', function(){
+let selectElement = document.querySelector('#glazing');
+for (var i = 0; i < glazeOptions.length; i++) {
+    let current = glazeOptions[i];
+    var option = document.createElement('option');
+    option.text = current.glaze;
+    option.value = i;
+    selectElement.add(option);
+    }
+// selectElement.addEventListener('change', onChange);
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+let selectElement2 = document.querySelector('#pack-size');
+for (var i = 0; i < packSizes.length; i++){
+    let current = packSizes[i];
+    var option = document.createElement('option');
+    option.text = current.packSize;
+    option.value = i;
+    selectElement2.add(option);
+    }
+// selectElement2.addEventListener('change', onChange);
+});
 
 
-function glazingChange(element) {
-    const priceChange = element.value;
-    // get value of selected glazing option
 
-    let packPrice = parseInt(document.querySelector("#packPrice").value);
-    //#packPrice to number
+function onChange(){
+    selectedGlazingIndex = selectElement.value;
+    selectedPackIndex = selectElement2.value;
+    let glazingPrice = parseFloat(glazeOptions[selectedGlazingIndex].priceAdd);
+    let packPrice = parseInt(packSizes[selectedPackIndex].priceMultiply);
+    let finalPrice = (2.49 + glazingPrice) * packPrice;
+    basePriceElement.innerText = finalPrice.toFixed(2);
+  }
 
-    let totalPrice = (basePrice + glazingPrice) * packPrice;
-    // add your code to do update the price ...
-}
+onChange();
 
-
-
-function displayGlaze(glaze) {
-    let carTitleElement = document.querySelector('#glaze');
-    let carInfoElement = document.querySelector('#car-info');
-
-    carTitleElement.innerText = car.model;
-    carInfoElement.innerText = car.description;
-}
-
-
-function onSelectValueChange() {
-    // In this function, `this` corresponds to the select
-    // element. So `this.value` will contain the value of the
-    // selected option as a string.
-    console.log(this.value);
-
-    // We need to convert the string value to an integer
-    let rollsIndex = parseInt(this.value);
-
-    // Now retrieve the object at the index specified by the select's value
-    let rollsToDisplay = allRolls[rollsIndex];
-
-    // Update the UI
-    displayRolls(rollsToDisplay);
-}
-
-
-let selectElement = document.querySelector('#rolls');
-//find the select element
-
-selectElement.addEventListener('change', onSelectValueChange);
-//event listener for when user changes the option
-
-displayRolls(allRolls[0]);
-// display the first roll
-
+selectElement.addEventListener('change', onChange);
+selectElement2.addEventListener('change', onChange);
